4740602fe0660d6147eb3f757e1f99183b060eee
soranerai <aire1@protonmail.com>
2026-07-13 20:55:17 +0300

    chore: release v2.1.1

--- lsposed/app/build.gradle.kts
+++ lsposed/app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         applicationId = "dev.soranerai.vpnhidenext"
         minSdk = 29
         targetSdk = 35
-        versionCode = 20100
+        versionCode = 20101
         versionName = buildVersion
 
         ndk {
